cd3f92e445e81d072669d0e64fb131dff963dca1
mathieu-hub <m.vancappellen@rubika-edu.com>
2024-09-24 21:14:25 +0200

    Start Resources Manager

---
+++ Assets/_SCRIPTS/MANAGER/ResourcesManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcesManager : MonoBehaviour
+{
+    public int credits = 0;
+    public int scraps = 0;
+    public int metal = 0;
+
+    public int citizen = 0;
+    public int globalSatisfaction = 0;
+    public int happiness = 0;
+    public int health = 0;
+    public int hungriness = 0;
+
+    public int structures = 0;
+    public int water = 0;
+    public int energy = 0;
+}
